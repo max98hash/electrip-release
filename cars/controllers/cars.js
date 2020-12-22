be196841034b83c7b4a4e31ece52672acc53d0c9
max98hash <maxime8198@gmail.com>
2020-12-22 22:27:50 +0100

    Add Basic Authentication

--- cars/controllers/cars.js
+++ cars/controllers/cars.js
@@ -1,9 +1,8 @@
 const Car = require('../models/car');
-const mongoose = require('mongoose');
 
 exports.getCars = (req, res, next) => {
 	Car.find()
-    .then(things => res.status(200).json(things))
+    .then(cars => res.status(200).json(cars))
     .catch(error => res.status(400).json({ error }));
 }
 
--- cars/routes/cars.js
+++ cars/routes/cars.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const cars = require('../controllers/cars');
 
+const auth = require('../middleware/auth');
+
 router.get('/', cars.getCars);
 
 router.post('/create', cars.createCars);
