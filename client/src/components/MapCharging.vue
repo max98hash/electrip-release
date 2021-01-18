58dd1cf20e9af4e92fd162b87865762f7f56370f
max98hash <maxime8198@gmail.com>
2021-01-18 18:01:22 +0100

    Cluster version of display of charging stations

--- client/src/components/MapCharging.vue
+++ client/src/components/MapCharging.vue
@@ -65,7 +65,7 @@ export default {
         let coordCharging = [];
         let lastPoint = points[0];
         for (let index = 1; index < nbPoints; index++) {
-            if( kmSinceLastCharge + this.getDistance(points[index],lastPoint) >= carAutonomy){
+            if( kmSinceLastCharge + this.getDistance(points[index],lastPoint) >= carAutonomy-10){
                 coordCharging.push(lastPoint);
                 kmSinceLastCharge = this.getDistance(points[index],lastPoint);
                 lastPoint = points[index]
@@ -107,7 +107,8 @@ export default {
         const geojson = await this.searchChargingStations(coordCharging);
         console.log("stations before display : ")
         console.log(geojson)
-        this.displayMarkersCharging(geojson);
+        //this.displayMarkersCharging(geojson);
+        this.displayerMarkersChargingClusterVersion(geojson)
 
     },
     async searchChargingStations(coordCharging){
@@ -128,7 +129,7 @@ export default {
                 const lat = station.AddressInfo.Latitude
                 const long = station.AddressInfo.Longitude
                 const comments = station.GeneralComments
-                geojson.features.push({
+                const stationJSON = {
                     type: 'Feature',
                     geometry: {
                         type: 'Point',
@@ -140,12 +141,24 @@ export default {
                         address,
                         comments
                     }
-                })
+                }
+                let alreadyExists = false
+                for (var i=0; i<geojson.features.length; i++) { //iterate through each object in an array
+                    if (JSON.stringify(geojson.features[i]) === JSON.stringify(stationJSON) ) {
+                            alreadyExists = true;
+                            break;
+                    }
+                }
+                if(!alreadyExists){
+                    geojson.features.push(stationJSON)
+                }
             })
             
         }
 
         return geojson;
+
+
 
     },
     async displayMarkersCharging(geojson){
@@ -170,6 +183,130 @@ export default {
         });
 
         this.setMarkersCharging(newMarkersCharging)
+    },
+    async displayerMarkersChargingClusterVersion(stations){
+
+        let map = this.getMapCharging;
+
+        if (map.getLayer('clusters')){
+            map.removeLayer('clusters');
+            console.log("Layer Removed")
+        }
+        if(map.getLayer('cluster-count')){
+            map.removeLayer('cluster-count');
+            console.log("Layer Removed")
+        }
+        if(map.getLayer('unclustered-point')){
+            map.removeLayer('unclustered-point');
+            console.log("Layer Removed")
+        }
+        if (map.getSource('stations')) {
+            map.removeSource('stations');
+            console.log("Source Removed")
+        }
+
+        map.addSource('stations', {
+            type: 'geojson',
+            // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+            // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+            data: stations,
+            cluster: true,
+            clusterMaxZoom: 14, // Max zoom to cluster points on
+            clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+        });
+
+        map.addLayer({
+            id: 'clusters',
+            type: 'circle',
+            source: 'stations',
+            filter: ['has', 'point_count'],
+            paint: {
+                // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+                // with three steps to implement three types of circles:
+                //   * Blue, 20px circles when point count is less than 100
+                //   * Yellow, 30px circles when point count is between 100 and 750
+                //   * Pink, 40px circles when point count is greater than or equal to 750
+                'circle-color': [
+                    'step',
+                    ['get', 'point_count'],
+                    '#51bbd6',
+                    3,
+                    '#f1f075',
+                    10,
+                    '#f28cb1',
+                    20,
+                    '#f5a73b'
+                ],
+                'circle-radius': [
+                    'step',
+                    ['get', 'point_count'],
+                    20,
+                    3,
+                    30,
+                    10,
+                    35,
+                    20,
+                    40
+                ]
+            }
+        });
+
+        map.addLayer({
+            id: 'cluster-count',
+            type: 'symbol',
+            source: 'stations',
+            filter: ['has', 'point_count'],
+            layout: {
+                'text-field': '{point_count_abbreviated}',
+                'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+                'text-size': 20
+            }
+        });
+
+        map.addLayer({
+            id: 'unclustered-point',
+            type: 'circle',
+            source: 'stations',
+            filter: ['!', ['has', 'point_count']],
+            paint: {
+                'circle-color': '#11b4da',
+                'circle-radius': 10,
+                'circle-stroke-width': 1,
+                'circle-stroke-color': '#fff'
+            }
+});
+
+        // When a click event occurs on a feature in
+        // the unclustered-point layer, open a popup at
+        // the location of the feature, with
+        // description HTML from its properties.
+        map.on('click', 'unclustered-point', function (e) {
+            var coordinates = e.features[0].geometry.coordinates.slice();
+            
+            // Ensure that if the map is zoomed out such that
+            // multiple copies of the feature are visible, the
+            // popup appears over the copy being pointed to.
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+
+            let properties = e.features[0].properties
+
+            console.log("Lat : "+e.lngLat.lat+" Long : "+e.lngLat.lng)
+            
+            new mapboxgl.Popup()
+                .setLngLat(coordinates)
+                .setHTML('<h3>' + properties.title + '</h3><p>' + properties.access + '</p><p>' + properties.address + '</p><p>' + properties.comments + '</p>')
+            .addTo(map);
+        });
+
+        map.on('mouseenter', 'clusters', function () {
+            map.getCanvas().style.cursor = 'pointer';
+        });
+        map.on('mouseleave', 'clusters', function () {
+            map.getCanvas().style.cursor = '';
+        });
+
     },
     async displayLayout(coordCharging,geometry,coordinates){
 
